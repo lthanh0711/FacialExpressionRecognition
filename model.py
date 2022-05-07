f36bd8bf1b3bfd3ce775e631c7b6c820ebd25ed6
lthanh <lthanh0711@gmail.com>
2022-05-07 21:45:56 +0700

    fix minor bug

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class FacialExpressionModel(object):
     def __init__(self, model_json_file, model_weights_file):
         with open(model_json_file, "r") as json_file:
             loaded_model_json = json_file.read()
-            self.loaded = model_from_json(loaded_model_json)
+            self.loaded_model = model_from_json(loaded_model_json)
             
         self.loaded_model.load_weights(model_weights_file)
         self.loaded_model.make_predict_function()
